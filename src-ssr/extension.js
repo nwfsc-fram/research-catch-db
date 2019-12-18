42241fbc45aaa6869088baa2b4202a958a09dc28
Sara Ogaz <sara.ogaz@noaa.gov>
2019-12-18 15:19:01 -0800

    final vuex for permits, and have close to final versions of main Permits page and User permit details page

--- src-ssr/extension.js
+++ src-ssr/extension.js
@@ -26,8 +26,9 @@ const getPermitsView = async (request, response) => {
         status: 401,
         message: error.message
       });
+    } else {
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 
@@ -39,25 +40,13 @@ const getGrouping = async (request, response) => {
         status: 401,
         message: error.message
       });
+    } else {
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 
-const getOrgID = async (request, response) => {
-  const orgName = request.params.orgname
-  pool.query('SELECT organization_id FROM "ORGANIZATION_LU" WHERE name =$1', [orgName], (error, results) => {
-    if (error) {
-      console.error(error.stack);
-      response.status(401).json({
-        status: 401,
-        message: error.message
-      });
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
+// Get the list of org names in the ORGANIZATION_LU table
 const getOrgNames = async (request, response) => {
   pool.query('SELECT name FROM "ORGANIZATION_LU"', (error, results) => {
     if (error) {
@@ -71,8 +60,110 @@ const getOrgNames = async (request, response) => {
   })
 }
 
-const getMaxResearchId = async (request, response) => {
-  pool.query('SELECT max(research_project_id) FROM "RESEARCH_PROJECT"', (error, results) => {
+// Get the maximum used research id value
+async function getMaxResearchId() {
+  let output = null;
+  try{
+    output = await pool.query('SELECT max(research_project_id) FROM "RESEARCH_PROJECT"');
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  return output.rows[0];
+}
+
+// Get an org id value for a given org name
+async function getOrgId(orgName) {
+  let output = null;
+  try{
+    output = await pool.query('SELECT organization_id FROM "ORGANIZATION_LU" WHERE name =$1', [orgName]);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  return output.rows[0];
+}
+
+// Get a user id value for a given email
+async function getUserId(email) {
+  let output = null;
+  try{
+    output = await pool.query('SELECT user_id FROM "USER" WHERE email_address =$1', [email]);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  return output.rows[0];
+}
+
+// Get the data_status_id from the data status look up
+async function getStatusId(status) {
+  let output = null;
+  try{
+    output = await pool.query('SELECT data_status_id FROM "DATA_STATUS_LU" WHERE status_string =$1', [status]);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  return output.rows[0];
+}
+
+/* 
+Update any permit entry. This function is picky about what the input 
+parameters are named and translates regular meaninful field values to 
+their id values in the DB tables when needed. 
+ */
+const updatePermit = async (request, response) => {
+  const entries = Object.entries(request.body)
+
+  let sqlString = 'UPDATE "RESEARCH_PROJECT" SET '
+
+  let orgReturn = null;
+  let userReturn = null;
+  let statusReturn = null;
+
+  // This function should be able to handle any possible changes to
+  // the research_project table, this means a lot of field handleing
+  // and translating values to ids 
+  for (const kset of entries) {
+    if (kset[0] === 'organization_name') {
+      try{
+        orgReturn = await getOrgId(kset[1]);
+        sqlString = sqlString.concat('organization_id=', orgReturn['organization_id'], ',');
+      } catch (err) {
+        console.log(err.stack);
+      }
+    }
+    else if (kset[0] === 'email_address') {
+      try{
+        userReturn = await getUserId(kset[1]);
+        sqlString = sqlString.concat('point_of_contact=', userReturn['user_id'], ',');
+      } catch (err) {
+        console.log(err.stack);
+      }
+    }
+    else if (kset[0] === 'data_status') {
+      try{
+        statusReturn = await getStatusId(kset[1]);
+        sqlString = sqlString.concat('data_status_id=', statusReturn['data_status_id'], ',');
+      } catch (err) {
+        console.log(err.stack);
+      }
+    }
+    // handle normal fields
+    else if (['permit_year', 'mortality_credits_applicable'].includes(kset[0])) {
+      sqlString = sqlString.concat(kset[0],'=',kset[1],',')
+    }
+    else if (kset[0] != 'permit_number') {
+      sqlString = sqlString.concat(kset[0],'=\'',kset[1],'\',')
+    }
+  }
+
+  sqlString = sqlString.slice(0, -1)
+  sqlString = sqlString.concat(' WHERE permit_number = \'',request.body.permit_number,'\'')
+
+  // Final query to update the row in the DB
+  pool.query(sqlString, [], (error, result) => {
     if (error) {
       console.error(error.stack);
       response.status(401).json({
@@ -80,34 +171,74 @@ const getMaxResearchId = async (request, response) => {
         message: error.message
       });
     }
-    response.status(200).json(results.rows)
+    // this isn't returning anything, not sure why since this example is in the docs...
+    else {
+      response.status(201).send(`Permit row updated: ${result.rows[0]}`)
+    }
   })
 }
 
-// Taking dates out, but ultimately need to put that back in
+/*
+This function can be used to add a new permit entry to
+the RESEARCH_PROJECT table. Does not require all fields
+to have values in the API call, it will automatically
+asign nulls to missing fields. Translates regular 
+meaningful field values to their id values when appropriate
+*/
 const addPermit = async (request, response) => {
   const result = {
-    researchProjectId: request.body.researchProjectId,
     permitNumber: request.body.permitNumber,
-    organizationId: request.body.organizationId,
+    organizationName: request.body.organizationName,
     projectName: request.body.projectName,
     permitYear: request.body.permitYear,
     startDate: request.body.startDate,
     endDate: request.body.endDate,
     mortalityCreditsApplicable: request.body.mortalityCreditsApplicable,
-    pointOfContact: request.body.pointOfContact,
+    email: request.body.email,
     dataStatus: request.body.dataStatus,
     deliveryDate: request.body.deliveryDate,
     issuedBy: request.body.issuedBy,
     principleInvestigator: request.body.principleInvestigator,
     notes: request.body.notes
   }
+  // Get a new unused number for the research_project_id value
+  let maxReturn = await getMaxResearchId();
+  let researchProjectId = Number(maxReturn['max']) + 1
+
+  // Translate values to Ids
+  let orgReturn = null;
+  let userReturn = null;
+  let statusReturn = null;
+  if (result.organizationName) {
+    try{
+      orgReturn = await getOrgId(result.organizationName);
+      result.organizationID = orgReturn['organization_id'];
+    } catch (err) {
+      console.log(err.stack);
+    }
+  }
+  if (result.email) {
+    try{
+      userReturn = await getUserId(result.email);
+      result.pointOfContact = userReturn['user_id'];
+    } catch (err) {
+      console.log(err.stack);
+    }
+  }
+  if (result.dataStatus) {
+    try{
+      statusReturn = await getStatusId(result.dataStatus);
+      result.dataStatusId = statusReturn['data_status_id'];
+    } catch (err) {
+      console.log(err.stack);
+    }
+  }
 
   pool.query('INSERT INTO "RESEARCH_PROJECT" VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
-    [result.researchProjectId, result.permitNumber, result.organizationId,
+    [researchProjectId, result.permitNumber, result.organizationId,
       result.projectName, result.permitYear, result.startDate,
       result.endDate, result.mortalityCreditsApplicable, result.pointOfContact,
-      result.dataStatus, result.deliveryDate, result.issuedBy,
+      result.dataStatusId, result.deliveryDate, result.issuedBy,
       result.principleInvestigator, result.notes], (error, result) => {
       if (error) {
         console.error(error.stack);
@@ -122,14 +253,6 @@ const addPermit = async (request, response) => {
       }
     })
 }
-
-// const updatePermit = (request, response) => {
-//   const { permitNum, updateFields } = request.body
-
-//   // Could do some error catching here for an empty updateFields
-//   sqlString = 'UPDATE "RESEARCH_PROJECT" SET project_name = \'regular size crabs\'
-//   WHERE permit_number = \'permit-04-2011\''
-// }
 
 const deletePermit = (request, response) => {
   const result = {
@@ -162,9 +285,8 @@ module.exports.extendApp = function ({ app, ssr }) {
   app.use(express.json());
   app.get('/api/permitsview', getPermitsView)
   app.get('/api/grouping', getGrouping)
-  app.get('/api/getOrgId/:orgname', getOrgID)
   app.get('/api/orgNames', getOrgNames)
-  app.get('/api/maxResearchId', getMaxResearchId)
   app.post('/api/permits', addPermit)
+  app.put('/api/permits', updatePermit)
   app.delete('/api/permits', deletePermit)
 }
--- src/pages/PermitDetailsUser.vue
+++ src/pages/PermitDetailsUser.vue
@@ -27,12 +27,12 @@
                   <div
                     class="self-center full-width no-outline"
                     tabindex="0"
-                  >{{ permit_info.pointOfContact }}</div>
+                  >{{ pointOfContact }}</div>
                 </template>
               </q-field>
               <q-field square outlined label="Email Address" stack-label class="bg-indigo-1">
                 <template v-slot:control>
-                  <div class="full-width no-outline">{{ permit_info.email }}</div>
+                  <div class="full-width no-outline">{{ email }}</div>
                 </template>
               </q-field>
             </div>
@@ -49,19 +49,19 @@
           <q-tab-panel name="permitDetails" class="bg-blue-4">
             <q-field outlined label="Permit Number" stack-label square class="bg-blue-1">
               <template v-slot:control>
-                <div class="full-width no-outline">{{ permit_info.permitNum }}</div>
+                <div class="full-width no-outline">{{ permit['permit_number'] }}</div>
               </template>
             </q-field>
 
             <q-field outlined label="Organization" stack-label class="bg-blue-1">
               <template v-slot:control>
-                <div class="full-width no-outline">{{ permit_info.organization }}</div>
+                <div class="full-width no-outline">{{ permit['organization_name'] }}</div>
               </template>
             </q-field>
 
             <q-field outlined label="Principle Investigator" stack-label class="bg-blue-1">
               <template v-slot:control>
-                <div class="full-width no-outline">{{ permit_info.pi }}</div>
+                <div class="full-width no-outline">{{ permit['principle_investigator'] }}</div>
               </template>
             </q-field>
           </q-tab-panel>
@@ -71,36 +71,17 @@
 
         <q-tab-panels v-model="tab" animated>
           <q-tab-panel name="permitDetails" class="bg-cyan-4">
-            <q-input
-              outlined
-              v-model="permit_info.cruiseStart"
-              label="Cruise Start"
-              stack-label
-              class="bg-cyan-1"
-              readonly
-            >
-              <q-menu>
-                <div class="row no-wrap q-pa-md">
-                  <q-date v-model="permit_info.cruiseStart" default-view="Years" />
-                </div>
-              </q-menu>
-            </q-input>
+            <q-field outlined label="Permit Start" stack-label class="bg-cyan-1">
+              <template v-slot:control>
+                <div class="full-width no-outline">{{ startDate }}</div>
+              </template>
+            </q-field>
 
-            <q-input
-              outlined
-              v-model="permit_info.cruiseEnd"
-              label="Cruise End"
-              stack-label
-              readonly
-              class="bg-cyan-1"
-              :rules="[val => val > permit_info.cruiseStart || 'Cruise end date must be after cruise start date']"
-            >
-              <q-menu>
-                <div class="row no-wrap q-pa-md">
-                  <q-date v-model="permit_info.cruiseEnd" default-view="Years" />
-                </div>
-              </q-menu>
-            </q-input>
+            <q-field outlined label="Permit End" stack-label class="bg-cyan-1">
+              <template v-slot:control>
+                <div class="full-width no-outline">{{ endDate }}</div>
+              </template>
+            </q-field>
           </q-tab-panel>
 
           <q-tab-panel name="catchData">
@@ -112,14 +93,16 @@
 
         <q-tab-panels v-model="tab" animated>
           <q-tab-panel name="permitDetails" class="bg-teal-4">
+            <font size="4">User Entry</font>
+            <br>
             <div>Notes</div>
-            <q-input v-model="permit_info.noteText" filled type="textarea" class="bg-teal-1" />
+            <q-input v-model="permitNotes" filled type="textarea" class="bg-teal-1" />
             <br />
             <q-card class="bg-teal-1 q-pa-md">
               <div class="row">
                 <p class="col-5">Mortality Credits Applicable:</p>
                 <q-btn-toggle
-                  v-model="permit_info.mortalityApplicable"
+                  v-model="permitMortality"
                   push
                   toggle-color="primary"
                   class="bg-blue-1"
@@ -144,47 +127,87 @@
     </div>
     <br />
 
-    <q-btn color="primary" class="justify-center">Save</q-btn>
+    <q-btn color="primary" class="justify-center" @click="updatePermitInfo">Save</q-btn>
+    <br>
+    <br>
+    <q-card class="bg-green" v-if="saveSuccesfulBlock"> <q-card-section><div>Save was successful</div></q-card-section></q-card>
+    <q-card class="bg-red" v-if="saveFailedBlock"> <q-card-section><div>Save unsucessful</div></q-card-section></q-card>
+    <br>
+    <div class="q-mt-md">{{ permit }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-//import { date } from 'quasar';
+import { date } from 'quasar';
 import axios from 'axios';
 
 @Component
 export default class Permits extends Vue {
-  permit_info = {
-    pointOfContact: 'Seric Ogaz',
-    email: 'trogdor@noaa.gov',
-    permitNum: '',
-    organization: null,
-    newOrganization: null,
-    pi: null,
-    cruiseStart: null,
-    cruiseEnd: null,
-    noteText: null,
-    mortalityApplicable: false
-  };
 
+  // to-do: poc and email will need to be hooked up to
+  // login acccount
+  pointOfContact = 'Seric Ogaz';
+  email = 'trogdor@noaa.gov';
   data = [];
-  temp = [];
 
   dense = false;
   filter = '';
   selected = [];
   tab = 'permitDetails';
   otherChosen = false;
+  saveSuccesfulBlock = false;
+  saveFailedBlock = false;
 
-  //save button needs to check if any values were actually updated
-  //and handle if org="other" carefully
+  updatePermitInfo() {
+    // Take out the point of contact field
+    let uploadObject = Object.assign(this.$store.state.sPermit.permit);
+    delete uploadObject['point_of_contact'];
 
-  created() {
-    axios
-      .get('http://localhost:8080/api/orgNames')
-      .then(response => (this.temp = response.data));
+    axios.put('http://localhost:8080/api/permits', uploadObject)
+      .then(res => {
+        console.log(res);
+        this.saveSuccesfulBlock = true;
+        this.saveFailedBlock = false;
+      })
+      .catch(error => {
+        console.log(error);
+        this.saveFailedBlock = true;
+        this.saveSuccesfulBlock = false;
+      })
+  }
+
+  get startDate() {
+    return date.formatDate(this.permit['start_date'], 'YYYY-MM-DD');
+  }
+
+  get endDate() {
+    return date.formatDate(this.permit['end_date'], 'YYYY-MM-DD');
+  }
+
+  get permitNotes() {
+    return this.$store.state.sPermit.permit.notes;
+  }
+
+  set permitNotes(value) {
+    this.$store.commit('sPermit/updateNotes', value);
+  }
+
+  get permitMortality() {
+    return this.$store.state.sPermit.permit.mortality_credits_applicable;
+  }
+
+  set permitMortality(value) {
+    this.$store.commit('sPermit/updateMortality', value);
+  }
+
+  get permit() {
+    return this.$store.state.sPermit.permit;
+  }
+
+  set permit(value) {
+    this.$store.commit('sPermit/updateSPermit', value)
   }
 }
 </script>
